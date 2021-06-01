e28bb399d162ba39b3c53001ffe7910d81ce9625
cloviscash <76001625+cloviscash@users.noreply.github.com>
2021-05-31 19:11:58 -0500

    fix devtools

--- src/store/index.js
+++ src/store/index.js
@@ -10,13 +10,16 @@ const persistedState = localStorage.getItem("state")
 
 // Create the saga middleware
 const sagaMiddleware = createSagaMiddleware();
+
+const devTools = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__() : null
+
 // Mount it on the Store
 const store = createStore(
   reducer,
   persistedState,
   compose(
     applyMiddleware(sagaMiddleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    devTools
   )
 );
 
